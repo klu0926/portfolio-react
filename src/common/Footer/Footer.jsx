4c38639919257c5fcf2b25946e9d1a62c3cb0700
klu0926 <lukuoyu@gmail.com>
2024-07-30 23:10:51 +0800

    feat: add server link

--- src/common/Footer/Footer.jsx
+++ src/common/Footer/Footer.jsx
@@ -1,12 +1,12 @@
 import Nav from 'react-bootstrap/Nav'
-
+import url from '../../data/url'
 // style
 import style from './footer.module.scss'
 
 // font awesome
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faGithub, faMedium } from '@fortawesome/free-brands-svg-icons'
-import { faFire } from '@fortawesome/free-solid-svg-icons'
+import { faServer } from '@fortawesome/free-solid-svg-icons'
 
 export default function Footer() {
   return (
@@ -33,6 +33,12 @@ export default function Footer() {
               <div className={style.linkDiv}>
                 <FontAwesomeIcon icon={faMedium} className={style.icon} />{' '}
                 Medium
+              </div>
+            </a>
+            <a target="_blank" className={style.link} href={url.server}>
+              <div className={style.linkDiv}>
+                <FontAwesomeIcon icon={faServer} className={style.icon} />{' '}
+                Server
               </div>
             </a>
           </Nav.Item>
