fdb338b9dbce0ad7387b0f734e39cd95988cb810
klu0926 <lukuoyu@gmail.com>
2024-08-01 17:29:01 +0800

    chat room WIP

--- src/components/SocketChat/SocketChat.jsx
+++ src/components/SocketChat/SocketChat.jsx
@@ -2,10 +2,17 @@ import url from '../../data/url'
 const server = url.server
 const socket = io.connect(server)
 import style from './socketChat.module.scss'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {
+  faCommentDots,
+  faTriangleExclamation,
+} from '@fortawesome/free-solid-svg-icons'
 
 import { useState, useEffect, useRef } from 'react'
 
 function SocketChat() {
+  const [name, setName] = useState('')
+  const [email, setEmail] = useState('')
   const [message, setMessage] = useState('')
   const [chat, setChat] = useState([])
   const [toggle, setToggle] = useState(false)
@@ -16,7 +23,10 @@ function SocketChat() {
     if (message.trim() === '') return
     const data = {
       from: socket.id,
+      name,
+      email,
       message,
+      date: new Date(),
     }
     socket.emit('sentMessage', data)
     // clean up message
@@ -60,57 +70,92 @@ function SocketChat() {
 
   return (
     <>
+      {/* toggle button */}
       <div
-        className={style.toggleButton}
+        className={`${style.toggleButton} ${toggle && style.active}`}
         onClick={() => {
           setToggle((old) => !old)
         }}
       >
-        {toggle ? 'true': 'false'}
+        <FontAwesomeIcon icon={faCommentDots} className={style.chatIcon} />
       </div>
-      <div className={style.chatRoomContainer}>
+      {/* chatroom container */}
+      <div className={`${style.chatRoomContainer} ${toggle && style.active}`}>
+        {/* name input */}
+        <input
+          type="text"
+          className={`form-control ${style.input}`}
+          id="name"
+          placeholder="name"
+          onChange={(e) => setName(e.target.value)}
+          autoComplete="off"
+        />
+        {/* email input */}
+        <input
+          type="email"
+          className={`form-control ${style.input}`}
+          id="email"
+          placeholder="email"
+          onChange={(e) => setEmail(e.target.value)}
+          autoComplete="off"
+        />
+        {/* chatroom */}
         <div className={style.chatRoom} ref={chatRoomRef}>
           {chat.map((c, index) => {
-            if (c.from === socket.id) {
+            if (c.from === 'server') {
+              // from server
+              return (
+                <>
+                  <div className={style.serverMessageDiv}>
+                    <p
+                      key={index}
+                      className={`${style.message} ${style.serverMessage}`}
+                    >
+                      <FontAwesomeIcon
+                        icon={faTriangleExclamation}
+                        className={style.serverIcon}
+                      />
+                      <span>{c.message}</span>
+                    </p>
+                  </div>
+                </>
+              )
+            } else if (c.from === 'lu') {
+              // from lu
+              return (
+                <>
+                  <div className={style.luMessageDiv}>
+                    <img src="/favicon/favicon-32x32.png" />
+                    <p
+                      key={index}
+                      className={`${style.message} ${style.luMessage}`}
+                    >
+                      <span>{c.message}</span>
+                    </p>
+                  </div>
+                </>
+              )
+            } else {
               // from myself
               return (
                 <p
                   key={index}
-                  className={`${style.message} ${style.sendMessage}`}
+                  className={`${style.message} ${style.myMessage}`}
                 >
                   <span>{c.message}</span>
-                </p>
-              )
-            } else if (c.from === 'server') {
-              // from server
-              return (
-                <p
-                  key={index}
-                  className={`${style.message} ${style.serverMessage}`}
-                >
-                  <span>{c.message}</span>
-                </p>
-              )
-            } else {
-              // from others
-              return (
-                <p
-                  key={index}
-                  className={`${style.message} ${style.receiveMessage}`}
-                >
-                  <span>{c.message}: receive</span>
                 </p>
               )
             }
           })}
         </div>
+        {/* message input */}
         <div className="input-group mb-2">
           <input
             ref={inputRef}
             onKeyDown={handleKeyDown}
             onChange={(e) => setMessage(e.target.value)}
             type="text"
-            className={`form-control ${style.messageInput}`}
+            className={`form-control ${style.input}`}
             placeholder="message..."
           />
           <button
